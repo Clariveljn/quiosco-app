6295462bc75a6a94e46df55c73eb298868cb4685
Clariveljn <cjeldres5@gmail.com>
2023-03-27 22:53:45 -0300

    Mostrando el Producto en el Modal

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Modal from "react-modal";
 import Sidebar from "@/components/Sidebar";
+import ModalProducto from "@/components/ModalProducto";
 import useQuiosco from "@/hooks/useQuiosco";
 
 const customStyles = {
@@ -36,7 +37,7 @@ export default function Layout({ children, pagina }) {
 
       {modal && (
         <Modal isOpen={modal} style={customStyles}>
-          <h1>Modal...</h1>
+          <ModalProducto />
         </Modal>
       )}
     </>
