b5d2b6fc80cf24e16c38d9157cc3a84264d17516
Clariveljn <cjeldres5@gmail.com>
2023-03-27 22:06:24 -0300

    Detectando el Producto que dimos Click

--- context/QuioscoProvider.js
+++ context/QuioscoProvider.js
@@ -6,6 +6,8 @@ const QuioscoContext = createContext();
 const QuioscoProvider = ({ children }) => {
   const [categorias, setCategorias] = useState([]);
   const [categoriaActual, setCategoriaActual] = useState({});
+  const [producto, setProducto] = useState({});
+  const [modal, setModal] = useState(false);
 
   const obtenerCategorias = async () => {
     const { data } = await axios("/api/categorias");
@@ -23,12 +25,25 @@ const QuioscoProvider = ({ children }) => {
     const categoria = categorias.filter((categ) => categ.id === id);
     setCategoriaActual(categoria[0]);
   };
+
+  const handleSetProducto = (producto) => {
+    setProducto(producto);
+  };
+
+  const handleChangeModal = () => {
+    setModal(!modal);
+  };
+
   return (
     <QuioscoContext.Provider
       value={{
         categorias,
         categoriaActual,
         handleClickCategoria,
+        producto,
+        handleSetProducto,
+        modal,
+        handleChangeModal,
       }}
     >
       {children}
