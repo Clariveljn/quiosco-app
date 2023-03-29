81b14a2d68df559b24d43fd85268a37dbfa59f30
Clariveljn <cjeldres5@gmail.com>
2023-03-29 14:33:15 -0300

    Eliminar productos del pedido

--- context/QuioscoProvider.js
+++ context/QuioscoProvider.js
@@ -60,6 +60,11 @@ const QuioscoProvider = ({ children }) => {
     setModal(!modal);
   };
 
+  const handleEliminarProducto = (id) => {
+    const pedidoActualizado = pedido.filter((producto) => producto.id !== id);
+    setPedido(pedidoActualizado);
+  };
+
   return (
     <QuioscoContext.Provider
       value={{
@@ -73,6 +78,7 @@ const QuioscoProvider = ({ children }) => {
         handleAgregarPedido,
         pedido,
         handleEditarCantidades,
+        handleEliminarProducto,
       }}
     >
       {children}
