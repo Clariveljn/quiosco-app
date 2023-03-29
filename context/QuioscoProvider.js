efac9308d788b9788457566e90961766c9ee296f
Clariveljn <cjeldres5@gmail.com>
2023-03-29 11:34:47 -0300

    Detectando la URL actual para recordar el progreso

--- context/QuioscoProvider.js
+++ context/QuioscoProvider.js
@@ -10,7 +10,6 @@ const QuioscoProvider = ({ children }) => {
   const [producto, setProducto] = useState({});
   const [modal, setModal] = useState(false);
   const [pedido, setPedido] = useState([]);
-  const [paso, setPaso] = useState(1);
 
   const obtenerCategorias = async () => {
     const { data } = await axios("/api/categorias");
@@ -55,10 +54,6 @@ const QuioscoProvider = ({ children }) => {
     setModal(false);
   };
 
-  const handleChangePaso = (paso) => {
-    setPaso(paso);
-  };
-
   return (
     <QuioscoContext.Provider
       value={{
@@ -71,8 +66,6 @@ const QuioscoProvider = ({ children }) => {
         handleChangeModal,
         handleAgregarPedido,
         pedido,
-        paso,
-        handleChangePaso,
       }}
     >
       {children}
