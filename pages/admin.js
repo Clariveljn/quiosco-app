d61c04b42a3e1068c721858114880278629c4699
Clariveljn <cjeldres5@gmail.com>
2023-03-31 00:09:29 -0300

    Consultando el Endpoint de Ordenes con useSWR

--- pages/admin.js
+++ pages/admin.js
@@ -1,6 +1,16 @@
+import useSWR from "swr";
+import axios from "axios";
 import AdminLayout from "@/layout/AdminLayout";
+import { cleanDistDir } from "@/next.config";
 
-export default function admin() {
+export default function Admin() {
+  const fetcher = () => axios("/api/ordenes").then((datos) => datos.data);
+  const { data, error, isLoading } = useSWR("/api/ordenes", fetcher);
+
+  console.log(data);
+  console.log(error);
+  console.log(isLoading);
+
   return (
     <AdminLayout pagina={"Admin"}>
       <h1 className="text-4xl font-black">Panel de Administración</h1>
