c1a0d2e396bd7954435a42c0a3c958eccb518f1b
Clariveljn <cjeldres5@gmail.com>
2023-03-30 23:40:59 -0300

    Crear un Endpoint para obtener las ordenes

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -2,6 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
+
+  //Obtener ordenes
+  const ordenes = await prisma.orden.findMany({
+    where: {
+      estado: false,
+    },
+  });
+  res.status(200).json(ordenes);
+
+  //Crear ordenes
   if (req.method === "POST") {
     const orden = await prisma.orden.create({
       data: {
@@ -11,6 +21,6 @@ export default async function handler(req, res) {
         fecha: req.body.fecha,
       },
     });
-    res.json(orden);
+    res.status(200).json(orden);
   }
 }
